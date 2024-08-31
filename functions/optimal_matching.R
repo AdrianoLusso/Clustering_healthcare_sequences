9c37173e01e29d01dfebc827c1933b9030cf319d
adriano <lussoadriano@gmail.com>
2024-08-31 11:25:05 -0700

    implementation of the R script

---
+++ functions/optimal_matching.R
@@ -0,0 +1,29 @@
+args <- commandArgs(trailingOnly = TRUE)
+
+input_dir <- args[1]
+output_dir <- args[2]
+
+
+# Verifica si el archivo de entrada existe
+if (!file.exists(input_dir)) {
+  stop("El archivo de entrada no existe: ", input_dir)
+}
+
+# Verifica si el directorio de salida es accesible
+output_dir_dir <- dirname(output_dir)
+if (!dir.exists(output_dir_dir)) {
+  stop("El directorio de salida no existe: ", output_dir_dir)
+}
+
+
+library("TraMineR")
+
+dataset <- read.csv(input_dir)
+
+sequences_matrix <- as.matrix(dataset[, c("element1", "element2", "element3", "element4", "element5", "element6", "element7")])
+    
+sequences <- seqdef(sequences_matrix)
+
+om = seqdist(sequences,method="OM",indel=1,sm="TRATE")
+
+write.csv(om,file=output_dir)
