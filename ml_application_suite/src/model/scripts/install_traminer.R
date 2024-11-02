65d7f2da0ea7513446c3a94020d18fb579ad6af2
Adriano Lusso <82339789+AdrianoLusso@users.noreply.github.com>
2024-11-02 15:31:18 -0300

    ch

--- ml_application_suite/src/model/scripts/install_traminer.R
+++ ml_application_suite/src/model/scripts/install_traminer.R
@@ -1,5 +1,6 @@
 if (!requireNamespace("TraMineR", quietly = TRUE)) {
-  options(repos = c(CRAN = "http://cran.r-project.org"))
+  #options(repos = c(CRAN = "http://cran.r-project.org"))
+  chooseCRANmirror()
   #install.packages("RColorBrewer")
   #install.packages("TraMineR")
 }
