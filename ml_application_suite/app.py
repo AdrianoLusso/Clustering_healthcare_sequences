8f5ee0fb2474af4926e9b29e7f1174edf91576bb
adriano <lussoadriano@gmail.com>
2024-11-02 10:43:08 -0700

    new script for installing traminer

--- ml_application_suite/app.py
+++ ml_application_suite/app.py
@@ -1,17 +1,21 @@
 import sys
 import os
+import subprocess
 import streamlit as st
+
 
 # Set the main project directory (main folder)
 MAIN_DIRECTORY = os.path.abspath(os.path.join(os.path.dirname(__file__), '../..'))
 # Add the main directory to sys.path so it can be accessed anywhere
 sys.path.append(MAIN_DIRECTORY)
 
+script_dir = os.path.dirname(__file__) + 'src/model/scripts/install_traminer.R'
+result = subprocess.run(['Rscript', script_dir],
+                        check=True, capture_output=True, text=True)
+
 from src.ui.screens.SequencesClustering import Screen_SequencesClustering
 from src.ui.screens.TimeframesStates import Screen_TimeframesStates
 from src.ui.screens.menu import Menu
-
-
 
 pages = {
         'Menu':[st.Page(Menu.create_Menu,title='Menu')],
