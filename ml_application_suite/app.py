4451fa5cf5af596a2fbcaed9232af7aa990e0203
Adriano Lusso <82339789+AdrianoLusso@users.noreply.github.com>
2024-11-02 11:10:55 -0700

    Update app.py

--- ml_application_suite/app.py
+++ ml_application_suite/app.py
@@ -10,8 +10,11 @@ MAIN_DIRECTORY = os.path.abspath(os.path.join(os.path.dirname(__file__), '../..'
 sys.path.append(MAIN_DIRECTORY)
 
 script_dir = os.path.dirname(__file__) + '/src/model/scripts/install_traminer.R'
-subprocess.run(['Rscript', script_dir],
+result=subprocess.run(['Rscript', script_dir],
                         check=True, capture_output=True, text=True)
+
+print("STDOUT:", result.stdout)
+print("STDERR:", result.stderr)
 
 from src.ui.screens.SequencesClustering import Screen_SequencesClustering
 from src.ui.screens.TimeframesStates import Screen_TimeframesStates
