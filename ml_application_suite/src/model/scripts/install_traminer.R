a70402c4113c08ab490380043e8b2d2b2bbb44ab
Adriano Lusso <82339789+AdrianoLusso@users.noreply.github.com>
2024-11-02 15:30:26 -0300

    changes

--- ml_application_suite/src/model/scripts/install_traminer.R
+++ ml_application_suite/src/model/scripts/install_traminer.R
@@ -1,4 +1,5 @@
 if (!requireNamespace("TraMineR", quietly = TRUE)) {
-  install.packages("RColorBrewer")
-  #install.packages("TraMineR", repos = "http://cran.r-project.org")
+  options(repos = c(CRAN = "http://cran.r-project.org"))
+  #install.packages("RColorBrewer")
+  install.packages("TraMineR")
 }
