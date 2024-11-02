579f3314b9cf1e8b521985ade8201b3e22921d40
Adriano Lusso <82339789+AdrianoLusso@users.noreply.github.com>
2024-11-02 18:47:34 -0300

    changes

--- ml_application_suite/src/model/scripts/install_traminer.R
+++ ml_application_suite/src/model/scripts/install_traminer.R
@@ -1,6 +1,6 @@
 if (!requireNamespace("TraMineR", quietly = TRUE)) {
     tryCatch({
-        install.packages("TraMineR", lib = "~/R/lib")
+        install.packages("TraMineR")
     }, error = function(e) {
         message("Error al instalar el paquete 'TraMineR': ", e$message)
         return(e)  # Retorna el objeto de la excepción
